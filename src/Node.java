3722fd5aeef275812b9f8186f26746aa83a3093a
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-01-21 12:54:22 -0800

    Added toolbox template

---
+++ src/Node.java
@@ -0,0 +1,25 @@
+package src;
+
+/**
+ * Represents a Node in a doubly linked list.
+ */
+public class Node {
+  /** The value stored in this node. */
+  public int value;
+
+  /** The previous node in the linked list. */
+  public Node prev;
+
+  /** The next node in the linked list. */
+  public Node next;
+
+  /**
+   * Creates a single node with the specified value.
+   * prev and next are kept as null.
+   *
+   * @param value The value to store in this node.
+   */
+  public Node(int value) {
+    this.value = value;
+  }
+}
---
+++ src/Toolbox.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Toolbox {
+
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
